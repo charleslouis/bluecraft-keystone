abc6a8f5d2865b5980aa2c1e2865f78216aa54f4
Kris Champ <krischamp@gmail.com>
2015-11-16 12:49:47 +0100

    modif gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,27 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
 var watch = require('gulp-watch');
-var shell = require('gulp-shell')
+var shell = require('gulp-shell');
 
-var sass = require('gulp-sass');
+var sass = require('gulp-sass'),
+    // autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 
 var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
-
-,
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json'],
 	'style': {
 		all: './public/styles/**/*.scss',
 		output: './public/styles/'
 	}
 
 };
+
 
 // gulp lint
 gulp.task('lint', function(){
@@ -28,31 +34,43 @@ gulp.task('lint', function(){
 		.pipe(jshint.reporter(jshintReporter));
 });
 
+
+ 
+gulp.task('sass:dev', function () {
+  gulp.src(paths.style.all)
+	.pipe(sourcemaps.init())
+	.pipe(sass({includePaths: [ './public/styles/foundation' ]}).on('error', sass.logError))
+	.pipe(gulp.dest(paths.style.output))
+	.pipe(livereload());
+});
+ 
+
+gulp.task('sass:build',function () {
+  gulp.src(paths.style.all)
+    .pipe(sass())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(minifycss())
+	.pipe(gulp.dest(paths.style.output));
+});
+
+
 // gulp watcher for lint
 gulp.task('watch:lint', function () {
 	gulp.src(paths.src)
 		.pipe(watch())
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter));
-});
-
-
+})
+;
 gulp.task('watch:sass', function () {
-	gulp.watch(paths.style.all, ['sass']);
-});
-
-gulp.task('sass', function(){
-	gulp.src(paths.style.all)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(paths.style.output));
+	livereload.listen();
+	gulp.watch(paths.style.all, ['sass:dev']);
 });
 
 
 gulp.task('runKeystone', shell.task('node keystone.js'));
 gulp.task('watch', [
-
   'watch:sass',
-
   'watch:lint'
 ]);
 
